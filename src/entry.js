427971e05540fcd7c111b6c8e852ff77a8092138
foxwhite <belgeomax@gmail.com>
2017-02-10 00:27:21 +0300

    add arrow down & smoothscroll

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,12 @@
 require('./sass/main.sass');
+var smoothScroll = require('smooth-scroll');
+
+
+smoothScroll.init({
+    selector: '[data-scroll]',
+    // selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+    speed: 800,
+    easing: 'easeInOutCubic',
+    offset: 0,
+    // callback: function ( anchor, toggle ) {} // Function to run after scrolling
+});
